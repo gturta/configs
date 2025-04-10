f97ba29dc6ef1a7cd87788c1b1a9b038965ad739
Gabriel Turta <gturta@gmail.com>
2025-04-10 13:12:25 +0300

    removed pylint

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -29,8 +29,8 @@ return {
       })
 
       local on_attach = function(client)
-    require'completion'.on_attach(client)
-end
+        require 'completion'.on_attach(client)
+      end
 
       lspconfig.rust_analyzer.setup({
         on_attach = on_attach,
@@ -61,25 +61,24 @@ end
           local client = vim.lsp.get_client_by_id(args.data.client_id)
 
           if client.supports_method('textDocument/hover') then
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {desc="Hover info about symbol"} )
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Hover info about symbol" })
           end
 
           if client.supports_method('textDocument/definition') then
-            vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, {desc="[G]oto [D]efinition"} )
+            vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, { desc = "[G]oto [D]efinition" })
           end
 
           if client.supports_method('textDocument/documentSymbol') then
-            vim.keymap.set('n', 'ds', require('telescope.builtin').lsp_document_symbols, {desc="[D]ocument [S]ymbols"} )
+            vim.keymap.set('n', 'ds', require('telescope.builtin').lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
           end
 
           if client.supports_method('textDocument/codeAction') then
-            vim.keymap.set({'n','x'}, '<leader>ca', vim.lsp.buf.code_action, {desc='[C]ode [A]ction'})
+            vim.keymap.set({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
           end
 
           if client.supports_method('textDocument/formatting') then
-            vim.keymap.set({'n','x'}, '<leader>gf', vim.lsp.buf.format, {desc='Buffer [F]ormat'})
+            vim.keymap.set({ 'n', 'x' }, '<leader>gf', vim.lsp.buf.format, { desc = 'Buffer [F]ormat' })
           end
-
         end
       })
     end
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,9 +6,7 @@ return {
 
 			null_ls.setup({
 				sources = {
-					null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.black,
-          null_ls.builtins.diagnostics.pylint,
 				},
 			})
 		end,
