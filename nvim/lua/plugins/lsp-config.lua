67c6c7552e0b63ee53c0fceb8d9a04f3e148a3ce
Gabriel Turta <gturta@gmail.com>
2025-04-11 11:21:59 +0300

    rust_analyzer fix

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -28,30 +28,8 @@ return {
         capabilities = capabilities
       })
 
-      local on_attach = function(client)
-        require 'completion'.on_attach(client)
-      end
-
       lspconfig.rust_analyzer.setup({
-        on_attach = on_attach,
-        settings = {
-          ["rust-analyzer"] = {
-            imports = {
-              granularity = {
-                group = "module",
-              },
-              prefix = "self",
-            },
-            cargo = {
-              buildScripts = {
-                enable = true,
-              },
-            },
-            procMacro = {
-              enable = true
-            },
-          }
-        }
+        settings = {}
       })
 
       vim.api.nvim_create_autocmd('LspAttach', {
