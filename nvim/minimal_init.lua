9c05fc3a9a524adec0ddc0bcd10bca6b6bc14010
Gabriel Turta <gturta@gmail.com>
2026-06-10 21:14:30 +0300

    removed markdown preview

--- nvim/minimal_init.lua
+++ nvim/minimal_init.lua
@@ -9,6 +9,8 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.g.have_nerd_font = true
@@ -33,9 +35,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-lua/plenary.nvim" },
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
   { src = "https://github.com/catppuccin/nvim" },
-  -- required by markdown-preview which comes next
-  { src = "https://github.com/selimacerbas/live-server.nvim" },
-  { src = "https://github.com/selimacerbas/markdown-preview.nvim" },})
+})
 
 
 -- ============================================================================
@@ -53,9 +53,7 @@ vim.lsp.config("rust_analyzer", {
   settings = {
     ["rust-analyzer"] = {
       check = { command = "clippy" },
-
       completion = { callable = { snippets = "fill_arguments", }, },
-
       inlayHints = {
         closingBraceHints = { enable = true, minLines = 25, },
         closureReturnTypeHints = { enable = "with_block", },
@@ -133,33 +131,29 @@ vim.api.nvim_create_autocmd("LspAttach", {
 vim.keymap.set("n", "<leader>ll", vim.diagnostic.setloclist, {
   desc = "Show diagnostic location list",
 })
-
 vim.keymap.set("n", "<leader>lc", vim.cmd.lclose, {
   desc = "Close diagnostic location list",
 })
-
 vim.keymap.set("n", "<M-j>", "<cmd>lnext<CR>", {
   desc = "Location list next line",
 })
-
 vim.keymap.set("n", "<M-k>", "<cmd>lprev<CR>", {
   desc = "Location list previous line",
 })
 
-vim.keymap.set("n", "<leader>vt", function()
+vim.keymap.set("n", "<leader>tt", function()
   local show_text = not vim.diagnostic.config().virtual_text
   vim.diagnostic.config({ virtual_text = show_text })
 end, {
   desc = "Toggle diagnostic virtual text",
 })
 
-vim.keymap.set("n", "<leader>vl", function()
+vim.keymap.set("n", "<leader>tl", function()
   local show_lines = not vim.diagnostic.config().virtual_lines
   vim.diagnostic.config({ virtual_lines = show_lines })
 end, {
   desc = "Toggle diagnostic virtual lines",
 })
-
 vim.keymap.set("n", "<leader>th", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, {
@@ -175,21 +169,10 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", {
 })
 
 -- ========= Telescope =========
-vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {
-  desc = "Find files",
-})
-
-vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {
-  desc = "Live grep",
-})
-
-vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {
-  desc = "Find buffers",
-})
-
-vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, {
-  desc = "Help tags",
-})
+vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, { desc = "Find files", })
+vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, { desc = "Live grep", })
+vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "Find buffers", })
+vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, { desc = "Help tags", })
 
 -- ========= Terminal window =========
 vim.keymap.set('n', '<leader>ti', function()
@@ -220,9 +203,4 @@ vim.keymap.set('t', '<C-w>h', [[<C-\><C-n><C-w>h]], { desc = 'Move to left windo
 vim.keymap.set('t', '<C-w>j', [[<C-\><C-n><C-w>j]], { desc = 'Move to bottom window' })
 vim.keymap.set('t', '<C-w>k', [[<C-\><C-n><C-w>k]], { desc = 'Move to top window' })
 vim.keymap.set('t', '<C-w>l', [[<C-\><C-n><C-w>l]], { desc = 'Move to right window' })
-
--- ========= Markdown Preview =========
-vim.keymap.set("n", "<leader>mps", "<cmd>MarkdownPreview<cr>", { desc = "Markdown: Start preview" })
-vim.keymap.set("n", "<leader>mpS", "<cmd>MarkdownPreviewStop<cr>", { desc = "Markdown: Stop preview" })
-vim.keymap.set("n", "<leader>mpr", "<cmd>MarkdownPreviewRefresh<cr>", { desc = "Markdown: Refresh preview" })
 
